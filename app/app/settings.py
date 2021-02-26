a54027f967724f7ccc99719c3af32c18fa5b5375
swill <swilltec@gmail.com>
2021-02-26 07:23:44 -0500

    mange tags

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     
     'user',
     'core',
+    'recipe',
 ]
 
 MIDDLEWARE = [
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test_user@gmail.com', password='testpass'):
+    """create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     email = "test@Gmail.com"
@@ -36,3 +43,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test the tag us a string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
